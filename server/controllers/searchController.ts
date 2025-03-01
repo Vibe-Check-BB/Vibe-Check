f821aa102a822524275674026d34dff4778d078b
Kristin Hooper <kristin.hooper13@gmail.com>
2025-03-01 17:16:09 -0500

    comment out mock data

--- server/controllers/searchController.ts
+++ server/controllers/searchController.ts
@@ -10,25 +10,25 @@ export const searchSongs = async (
   try {
     const { userQuery } = req.body;
     if (!userQuery) throw new Error('User query must be provided');
-    // const queryEmbedding = await getEmbedding(userQuery);
-    // if ((queryEmbedding.length = 0))
-    //   throw new Error('Failed to generate embeddings on user query');
-    // const similarSongs = await findSimilarSongs(queryEmbedding);
-    // res.locals.similarSongs = similarSongs;
-    res.locals.similarSongs = [
-      {
-        title: 'Mock Song 1',
-        artist: 'Mock Artist',
-        genre: 'Rock',
-        score: 0.95,
-      },
-      {
-        title: 'Mock Song 2',
-        artist: 'Mock Artist',
-        genre: 'Jazz',
-        score: 0.89,
-      },
-    ];
+    const queryEmbedding = await getEmbedding(userQuery);
+    if ((queryEmbedding.length = 0))
+      throw new Error('Failed to generate embeddings on user query');
+    const similarSongs = await findSimilarSongs(queryEmbedding);
+    res.locals.similarSongs = similarSongs;
+    // res.locals.similarSongs = [
+    //   {
+    //     title: 'Mock Song 1',
+    //     artist: 'Mock Artist',
+    //     genre: 'Rock',
+    //     score: 0.95,
+    //   },
+    //   {
+    //     title: 'Mock Song 2',
+    //     artist: 'Mock Artist',
+    //     genre: 'Jazz',
+    //     score: 0.89,
+    //   },
+    // ];
     return next();
   } catch (err) {
     return next({
