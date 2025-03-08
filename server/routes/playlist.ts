740df64fdb88931d5662dacf93766607ba759b22
Mickael Contreras <mickaelcontreras508@gmail.com>
2025-03-07 22:04:59 -0800

    frontend connection in progress...

--- server/routes/playlist.ts
+++ server/routes/playlist.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { Request, Response, NextFunction } from 'express';
+import { searchSongs } from '../controllers/searchController.js';
 import { getUserId, createPlaylist, addSongsToPlaylist } from '../middleware/createUserPlaylist.ts';
 const router = express.Router();
 
 
-router.post("/create-playlist", async (req, res) => {
-    const { accessToken, playlistName, trackUris } = req.body;
+router.post("/create-playlist", searchSongs, async(req, res) => {
+    const { accessToken, } = req.body;
+    const playlistName = "Vibe Check playlist"
     
     if (!accessToken) return res.status(400).json({ error: " Access token not found in req.body" });
   
@@ -17,12 +18,18 @@ router.post("/create-playlist", async (req, res) => {
         const playlistId = await createPlaylist(accessToken, userId, playlistName);
         console.log('playlist created id:', playlistId)
 
+        const similarSongs = res.locals.similarSongs;
+
+        console.log('similarSongs:', similarSongs)
+
+        const trackUris = similarSongs.map((song:any ) => `spotify:track:${song.spotifyId}`)
+
         if (trackUris && trackUris.length > 0) {
             await addSongsToPlaylist(accessToken, playlistId, trackUris);
             console.log('Songs added to Playlist');
         }
 
-        res.json({message: `created playlist Id:, ${playlistId}`})
+        res.json({message: `created playlist with Id:, ${playlistId}`})
         
 
     } catch (error: any) {
